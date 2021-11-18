126d3103f88475f09e110aa8c6e2e26088af8369
eugeneskom <eugeneskom@gmail.com>
2021-11-18 11:11:02 +0200

    swiper

--- app/js/main.js
+++ app/js/main.js
@@ -1,24 +1,9 @@
 $(function () {
-  const swiper = new Swiper('.swiper', {
+  const swiper = new Swiper('.nav-container__content--popular', {
     // Optional parameters
     direction: 'vertical',
     loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
+
   });
 
 }); // page loaded
